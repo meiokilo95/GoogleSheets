f0c229962bc5e255efc4338b9710be43c2ca893b
Cristian Garza Garcia <cgarcia@paystand.com>
2022-04-29 17:25:58 -0700

    cambios de cards

--- connect-sheets/src/components/Card.js
+++ connect-sheets/src/components/Card.js
@@ -1,13 +1,9 @@
 import React from 'react'
 
-function Card({Modelo}) {
+function Card({}) {
   return (
-    <div className='card text-center'>
-      <img src=""/>
-        <div className='card-body text-light'>
-        <h4 className='card-title'>{Modelo} </h4>
-        <p>Lorem Ipsu</p>
-        </div>
+    <div className=''>
+      
     </div>
   )
 }
--- connect-sheets/src/components/Cards.js
+++ connect-sheets/src/components/Cards.js
@@ -1,31 +1,48 @@
-import React from 'react'
+import React, {useLayoutEffect, useState, useEffect} from 'react'
 
 import Card from './Card'
 
 import useGoogleSheets from 'use-google-sheets';
 
 function Cards() {
-  const { data } = useGoogleSheets({
-    apiKey: 'AIzaSyB5nx15bNw10Y5YTlinwMElO3DQhLDbmKI',
-    sheetId: '1FyBkFmdLO8BeNdmDohYRvAh_nJP1jsdsEZ_rPYm8m1s',
-  });
 
+    const { data, loading, error } = useGoogleSheets({
+      apiKey: 'AIzaSyB5nx15bNw10Y5YTlinwMElO3DQhLDbmKI',
+      sheetId: '1FyBkFmdLO8BeNdmDohYRvAh_nJP1jsdsEZ_rPYm8m1s',
 
-  const datos = data[0];
+    }); 
 
-  console.log(datos);
+    
 
+    const [ casa, setCasas] = useState()
+
+    useEffect(() => {
+
+    if (data[0] !== undefined && 'data' in data[0]) {
+      setCasas([
+        data[0].data 
+      ])
+    }
+      
+    },[data] );
+    
+
+    if (loading) {
+      return <div>Loading...</div>;
+    }
   
+    if (error) {
+      return <div>Error!</div>;
+    }
+  
+    
+    console.log(data)
+
   return (
     
     <div className='container'>
       <div className='row'> 
         {
-          datos.map(card => (
-            <div className='col-md-4' key={card.id}>
-                <Card title={card.Modelo}  />
-            </div>
-          ))
         }
       </div>
     </div>
